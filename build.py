2841e2264515383db93ab3be0a3828f657c9f9ee
FranciscoOssian <ossian.neto@gmail.com>
2023-06-12 18:02:22 -0300

    add3

---
+++ build.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('cd model && wget https://s3.amazonaws.com/ir_public/ai/nsfw_models/nsfw.299x299.h5 && cd ..')
+
+os.system('python3 install -r requirements.txt')
--- start.py
+++ start.py
@@ -1,5 +1,3 @@
 import os
 
-os.system('cd model && wget https://s3.amazonaws.com/ir_public/ai/nsfw_models/nsfw.299x299.h5 && cd ..')
-
 os.system('gunicorn main:app')
