b2019f60469157cb4a9a96f086424d6c7cf46d75
FranciscoOssian <ossian.neto@gmail.com>
2023-06-13 17:39:58 -0300

    add

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd model
-wget https://s3.amazonaws.com/ir_public/ai/nsfw_models/nsfw.299x299.h5
-cd ..
-
-pip install -r requirements.txt
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-lsof -i :8000 | awk 'NR>1 {print $2}' | xargs kill
-
-#export PYTHON_SERVER_KEY=value
-gunicorn main:app
