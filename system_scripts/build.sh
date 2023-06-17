ab8610f70b32f034a4a3131ab70184b653084ba8
FranciscoOssian <ossian.neto@gmail.com>
2023-06-17 11:49:52 -0300

    add

--- system_scripts/build.sh
+++ system_scripts/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-cd ..
-cd model
-wget https://s3.amazonaws.com/ir_public/ai/nsfw_models/nsfw.299x299.h5
-cd ..
+# Get the absolute directory of the script
+script_dir=$(dirname "$(realpath "$0")")
 
-pip3 install -r requirements.txt
+# Navigate to the project root from the script's directory
+project_root="$script_dir/.."
+
+# Download a file to the models directory
+wget -P "$project_root/models" https://s3.amazonaws.com/ir_public/ai/nsfw_models/nsfw.299x299.h5
+
+# Install the dependencies listed in requirements.txt
+pip install -r "$project_root/requirements.txt"
